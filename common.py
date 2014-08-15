443e00d585e073fce41e0286b994da10e93f98cb
Elephant Liu <lexdene@sohu.com>
2014-08-15 17:20:06 +0800

    maybe Exchange type is useless

--- common.py
+++ common.py
@@ -10,7 +10,7 @@ print('uid = {}'.format(uid))
 exchange_name='user_{}'.format(uid)
 print('exchange name: {}'.format(exchange_name))
 
-exchange = Exchange(exchange_name, 'fanout', durable=True, auto_delete=True)
+exchange = Exchange(exchange_name, durable=True, auto_delete=True)
 
 # maybe i can use a temporary queue here
 queue = Queue(utils.uuid(), exchange=exchange, exclusive=True, auto_delete=True)
